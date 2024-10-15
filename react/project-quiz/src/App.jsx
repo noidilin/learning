40b94faed6740acd9455c182f260c222ab064dae
noidilin <linganinja.0120@gmail.com>
2024-10-15 14:40:08 +0800

    chore: init project

---
+++ react/project-quiz/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+import Header from './Header';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
