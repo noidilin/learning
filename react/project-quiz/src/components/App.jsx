1f97f26e7583350cedefaf1d17f6ccff64c113f9
noidilin <linganinja.0120@gmail.com>
2024-10-16 19:25:03 +0800

    feat: add start and finish screen

---
+++ react/project-quiz/src/components/App.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useReducer } from 'react';
+
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import FinishScreen from './FinishScreen';
+
+const initialState = {
+  questions: [],
+  status: 'loading', // 'loading', 'error', 'ready', 'active', 'finished'
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondsRemaining: null,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    case 'start':
+      return {
+        ...state,
+        status: 'active',
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
+    case 'finish':
+      return {
+        ...state,
+        status: 'finished',
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
+function App() {
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0,
+  );
+
+  useEffect(() => {
+    fetch('http://localhost:9000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed', payload: err }));
+    return () => {};
+  }, []);
+
+  return (
+    <div className='app'>
+      <Header />
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'finished' && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/project-quiz/src/components/StartScreen.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+
+function StartScreen({ numQuestions, dispatch }) {
+  return (
+    <div className='start'>
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>{numQuestions} questions to test your React mastery</h3>
+      <button
+        className='btn btn-ui'
+        onClick={() => dispatch({ type: 'start' })}
+      >
+        start
+      </button>
+    </div>
+  );
+}
+export default StartScreen;
