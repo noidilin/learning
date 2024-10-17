eb9a62cdd30c272770b039201e094438c3886053
noidilin <linganinja.0120@gmail.com>
2024-10-17 14:57:39 +0800

    feat: basic routing

---
+++ react/project-worldwise/src/components/CityList.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import styles from './CityList.module.css';
+import Spinner from './Spinner';
+import CityItem from './CityItem';
+import Message from './Message';
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message='add your first city by clicking on a city on the map.' />
+    );
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem key={city.id} city={city} />
+      ))}
+    </ul>
+  );
+}
+export default CityList;
