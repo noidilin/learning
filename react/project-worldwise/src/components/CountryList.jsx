7b4992bd938de5b4bc0849680ca54eb29d77c7a9
noidilin <linganinja.0120@gmail.com>
2024-10-18 15:47:31 +0800

    feat: add map feature

--- react/project-worldwise/src/components/CountryList.jsx
+++ react/project-worldwise/src/components/CountryList.jsx
@@ -1,10 +1,12 @@
-/* eslint-disable react/prop-types */
 import styles from './CountryList.module.css';
 import Spinner from './Spinner';
 import CountryItem from './CountryItem';
 import Message from './Message';
+import { useCities } from '../contexts/CitiesContext';
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
