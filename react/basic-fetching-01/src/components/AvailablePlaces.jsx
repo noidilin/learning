6163b6058593c75cc5b7af0198a4ca65de10a366
noidilin <linganinja.0120@gmail.com>
2024-10-23 23:22:00 +0800

    feat: add fetch user and available place

--- react/basic-fetching-01/src/components/AvailablePlaces.jsx
+++ react/basic-fetching-01/src/components/AvailablePlaces.jsx
@@ -1,10 +1,59 @@
-import Places from './Places.jsx';
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+
+import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
+  // NOTE: fetch available places from server
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchAvailablePlaces();
+
+        // NOTE: although getCurrentPosition() is not synchronous, we can't use await keyword at it
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          setAvailablePlaces(sortedPlaces);
+          // NOTE: therefore setIsFetching needs to be placed at the end of the callback of getCurrentPosition
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+        // NOTE: in addition we need a setIsFetching here as well
+        setIsFetching(false);
+      }
+      // NOTE: if place at here, setIsFetching won't wait getCurrentPosition finished.
+      // setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
