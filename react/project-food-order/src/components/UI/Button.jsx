4dd9e70fc2dbb9b063e34793ca4dad8fdc7d9690
noidilin <linganinja.0120@gmail.com>
2024-10-24 15:17:01 +0800

    feat: markup basic UI

---
+++ react/project-food-order/src/components/UI/Button.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+
+function Button({ children, textOnly, className, ...props }) {
+  let cssClasses = textOnly ? 'text-button' : 'button';
+  cssClasses += ' ' + className;
+  return (
+    <button className={cssClasses} {...props}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
