73ce4ba1b857d6f21846cd2e2cf1aea969e38dc1
noidilin <linganinja.0120@gmail.com>
2024-10-24 13:37:19 +0800

    feat: add input validation
    validation with state, onBlur, browser required

--- react/basic-form-01/src/components/Login.jsx
+++ react/basic-form-01/src/components/Login.jsx
@@ -1,23 +1,43 @@
+import { useRef, useState } from 'react';
+
 export default function Login() {
+  const [emailIsValid, setEmailIsValid] = useState(true);
+
+  const email = useRef();
+  const password = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // NOTE: reset value with useRef is harder that useState
+    // email.current.value = '' is not recommended.
+    const enteredEmail = email.current.value;
+    const enteredPassword = email.current.value;
+
+    setEmailIsValid(enteredEmail.includes('@'));
+
+    console.log(enteredEmail, enteredPassword);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+      <div className='control-row'>
+        <div className='control no-margin'>
+          <label htmlFor='email'>Email</label>
+          <input id='email' type='email' name='email' ref={email} />
         </div>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+        <div className='control no-margin'>
+          <label htmlFor='password'>Password</label>
+          <input id='password' type='password' name='password' ref={password} />
         </div>
       </div>
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+      <p className='form-actions'>
+        <button className='button button-flat'>Reset</button>
+        <button className='button'>Login</button>
       </p>
     </form>
   );
