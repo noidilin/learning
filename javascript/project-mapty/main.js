a3810bb54c2d42009acf3fee19d23b50d2c45939
noidilin <linganinja.0120@gmail.com>
2024-10-25 11:35:20 +0800

    chore: init project

---
+++ javascript/project-mapty/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import './style.css';
+import icon from './src/icon.png';
+import logo from './src/logo.png';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
