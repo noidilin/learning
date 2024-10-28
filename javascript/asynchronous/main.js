ab63bd3e345306f1dd3cf2b781842e7fa05510be
noidilin <linganinja.0120@gmail.com>
2024-10-28 23:29:18 +0800

    chore: init project

---
+++ javascript/asynchronous/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import './style.css';
+// import { setupCounter } from './counter.js'
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
