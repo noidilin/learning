5bebeeda2508fceb5ee0a8299a74a244afdba68d
noidilin <linganinja.0120@gmail.com>
2024-11-14 17:38:11 +0800

    chore: setup basic routing

--- react/router-01/src/App.jsx
+++ react/router-01/src/App.jsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './pages/Root';
+import Home from './pages/Home';
+import Products from './pages/Products';
+
+// the classic way of create route
+const router = createBrowserRouter([
+  {
+    // acts like a path dependent layout wrapper
+    path: '/',
+    element: <Root />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/products', element: <Products /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
