112efe510871c0c55b0947860ab48e3f77f29b72
noidilin <linganinja.0120@gmail.com>
2025-02-16 22:53:52 +0800

    chore: init project
    - setup with tailwind v4.0
    - new vite.config.js for tailwind v4.0
    - new eslint.config.js
    - new prettierrc

---
+++ react/project-tictac/src/App.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+
+// import { useState } from 'react';
+
+function Board() {
+  return (
+    <>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Board />
+    </>
+  );
+}
+
+export default App;
