c27c6c48c2d979118fdaf3e830b27a0b0b0f8f92
noidilin <linganinja.0120@gmail.com>
2025-03-24 01:03:52 +0800

    feat(error): add error notification when fetch failed

--- react/project-pizza/src/App.jsx
+++ react/project-pizza/src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
 import Home from './ui/Home';
+import NotFound from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
         path: '/menu',
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <NotFound />,
       },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
