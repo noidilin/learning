6ee7efb6e19ec8b2421c1c4bc7bdc00c434d7f38
noidilin <linganinja.0120@gmail.com>
2025-03-23 12:24:37 +0800

    refactor: organize import and Provider

--- react/redux-03/src/main.jsx
+++ react/redux-03/src/main.jsx
@@ -1,16 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.css';
+import "./index.css";
 
-import store from './store/store';
-import App from './App.jsx';
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
 );
