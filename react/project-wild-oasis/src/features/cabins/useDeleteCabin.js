70bb7e0f761ff79e1732141e20af33af194f7d5d
noidilin <linganinja.0120@gmail.com>
2025-04-25 22:40:03 +0800

    refactor(form): add compound component patter

--- react/project-wild-oasis/src/features/cabins/useDeleteCabin.js
+++ react/project-wild-oasis/src/features/cabins/useDeleteCabin.js
@@ -1,17 +1,17 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { toast } from 'react-hot-toast';
-import { deleteCabin as deleteCabinApi } from '../../services/apiCabins';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { deleteCabin as deleteCabinApi } from "../../services/apiCabins";
 
 export function useDeleteCabin() {
   const queryClient = useQueryClient();
 
-  const { isPending: isDeleting, mutate: deleteCabin } = useMutation({
+  const { isLoading: isDeleting, mutate: deleteCabin } = useMutation({
     mutationFn: deleteCabinApi,
     onSuccess: () => {
-      toast.success('Cabin successfully deleted');
+      toast.success("Cabin successfully deleted");
 
       queryClient.invalidateQueries({
-        queryKey: ['cabins'],
+        queryKey: ["cabins"],
       });
     },
     onError: (err) => toast.error(err.message),
