7bbc6cb1b149b36b99d8b9732ac4525c49ebbec3
noidilin <linganinja.0120@gmail.com>
2025-08-27 23:08:23 +0800

    feat(render): capture and render task

--- typescript/ts-basic/src/main.ts
+++ typescript/ts-basic/src/main.ts
@@ -16,6 +16,35 @@ function addTask(task: Task): void {
 }
 
 function renderTask(task: Task): void {
+  // create: list
+  const taskElement = document.createElement('li');
+  taskElement.textContent = task.description;
+  // render
+  taskListElement?.appendChild(taskElement);
 }
+
+const taskForm = document.querySelector<HTMLFormElement>('.form');
+const formInput = document.querySelector<HTMLInputElement>('.form-input');
+const taskListElement = document.querySelector<HTMLUListElement>('.list');
+
+taskForm?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskDescription = formInput?.value;
+  // receive input
+  if (taskDescription) {
+    const task: Task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+    // add, render and store task
+    addTask(task);
+    renderTask(task);
+    updateStorage();
+    // reset input
+    formInput.value = '';
+    return;
+  }
+  alert('Please enter a task description');
+});
 
 tasks.forEach(renderTask);
