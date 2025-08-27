e7e730624506a930d2e92e65b3c8b3229a2e19cf
noidilin <linganinja.0120@gmail.com>
2025-08-27 23:18:19 +0800

    feat(form): form submission

--- typescript/ts-react/src/App.tsx
+++ typescript/ts-react/src/App.tsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react"
+import { type Task } from "./component/types"
+import Form from "./component/Form"
+
+function loadTasks(): Task[] {
+  const storedTasks = localStorage.getItem('tasks')
+  return storedTasks ? JSON.parse(storedTasks) : []
+}
+
+function updateStorage(tasks: Task[]) {
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
 export default function App() {
+  const [tasks, setTasks] = useState<Task[]>(() => loadTasks());
+
+  const addTask = (task: Task) => setTasks([...tasks, task])
+
+  useEffect(() => {
+    updateStorage(tasks)
+  }, [tasks])
+
   return (
     <section>
+      <Form addTask={addTask} />
     </section>
   )
 }
