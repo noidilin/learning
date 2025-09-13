8ad481cb9a7ec890245fbe58af19ab0f86c6660c
noidilin <linganinja.0120@gmail.com>
2025-09-14 00:25:31 +0800

    feat(form): edit invoice feature

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,7 +7,7 @@ import { redirect } from "next/navigation";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-const FromSchema = z.object({
+const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
   amount: z.coerce.number(),
@@ -15,7 +15,7 @@ const FromSchema = z.object({
   date: z.string(),
 })
 
-const CreateInvoice = FromSchema.omit({ id: true, date: true })
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -33,3 +33,24 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices')
 }
 
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
