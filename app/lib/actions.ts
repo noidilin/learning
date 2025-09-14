b9f282fb76798f293b3ccd32ed8ab2262db2b822
noidilin <linganinja.0120@gmail.com>
2025-09-14 14:06:46 +0800

    feat(auth): add login and logout feature

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import z from "zod"
 import { revalidatePath } from "next/cache";
 import postgres from 'postgres';
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -99,4 +101,22 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined, formData: FormData
+) {
+  try {
+    await signIn('credentials', formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'invalid credentials'
+        default:
+          return 'something went wrong'
+      }
+    }
+    throw error
+  }
 }
