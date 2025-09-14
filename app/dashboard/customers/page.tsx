6645c58d49c4cfd6997be70449e249d6ddf6ec70
noidilin <linganinja.0120@gmail.com>
2025-09-14 14:15:05 +0800

    feat(metadata): add metadata to each page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customer',
+};
+
 function Page() {
   return (
     <p>Customers Page</p>
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 // on the server side, page components accept a prop called `searchParams` automatically
 export default async function Page(props: { searchParams?: Promise<{ query?: string; page?: string }> }) {
